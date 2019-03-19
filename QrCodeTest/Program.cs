bec683011adbc601edcc8b7bb532f88a08de7b24
B Q <qbb1998@outlook.com>
2019-03-19 23:50:45 +0800

    QrCodeTest

--- QrCodeTest/Program.cs
+++ QrCodeTest/Program.cs
@@ -3,13 +3,101 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Gma.QrCodeNet.Encoding;
+using Gma.QrCodeNet.Encoding.Windows.Render;
+using System.Drawing;
+ using System.Drawing.Imaging;
+ using System.IO;
 
 namespace QrCodeTest
 {
     class Program
     {
+        //将文本文件每行的字符串信息生成QrCode
+        static void fileToQrCode(string file)
+        {
+            string[] text = file.Split('\n');
+            for (int i = 0; i < text.Length; i++)
+            {
+                QrEncoder qrEncoder = new QrEncoder();
+                QrCode qrCode = qrEncoder.Encode(text[i]);
+                string filename;
+                if(text[i].Length >= 4)
+                {   //生成的QrCode以信息所在行号三位数+信息的前四个字符构成
+                    filename = (i + 1).ToString("X3") + text[i].Substring(0, 4) + ".png";
+                }
+                else
+                {
+                    filename = (i + 1).ToString("X3") + text[i] + ".png";
+                }
+                GraphicsRenderer render = new GraphicsRenderer(new FixedModuleSize(5, QuietZoneModules.Two), Brushes.Black, Brushes.White);
+                using (FileStream stream = new FileStream(filename, FileMode.Create))
+                {
+                    render.WriteToStream(qrCode.Matrix, ImageFormat.Png, stream);
+                }
+            }
+        }
+        //直接在控制台上用黑白方块打印出QrCode码
+        static void printQrCode(string str)
+        {
+            QrEncoder qrEncoder = new QrEncoder();
+            QrCode qrCode = qrEncoder.Encode(str);
+            for (int i = 0; i < qrCode.Matrix.Width + 2; i++)
+            {
+                Console.Write("█");
+            }
+            Console.WriteLine();
+            for (int j = 0; j < qrCode.Matrix.Width; j++)
+            {
+                Console.Write("█");
+                for (int i = 0; i < qrCode.Matrix.Width; i++)
+                {
+                    char charToPrint = qrCode.Matrix[j, i] ? '　' : '█';
+                    Console.Write(charToPrint);
+                }
+                Console.Write("█");
+                Console.WriteLine();
+            }
+            for (int i = 0; i < qrCode.Matrix.Width + 2; i++)
+            {
+                Console.Write("█");
+            }
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
+            if(args.Length != 0)
+            {
+                if(args[0].Length > 100)
+                {   //如果命令参数长度超过100，输出提示
+                    Console.WriteLine("{0} You can't input more than 100 characters!", args[0].Length);
+                }
+                else
+                {
+                    if (args[0].Substring(0, 2) == "-f" && args[0].Length > 2)
+                    {
+                        if (File.Exists(args[0].Substring(2)))
+                        {
+                            string file = File.ReadAllText(args[0].Substring(2));
+                            fileToQrCode(file);
+                        }
+                        else
+                        {//如果参数文件名错误，输出提示
+                            Console.WriteLine("The file does not exist,please write a correct file name!");
+                        }
+                    }
+                    else
+                    {
+                        printQrCode(args[0]);
+                    }
+                }
+                
+            }
+            else
+            {   //如果没有参数，输出提示
+                Console.WriteLine("You need to input something.");
+            }
         }
     }
 }
---
+++ QrCodeTest/README.md
@@ -0,0 +1,32 @@
+## CSharp QrCodeTest
+
+#### 功能概述
+
+通过在命令行输入参数，生成QrCode。
+
+#### 项目特色
+
+1.在命令行输入参数（长度不要超过100个字符）为所需生成QrCode时，会在控制台屏幕上输出对应黑、白字符方块组成的QrCode码，如下图所示：
+
+![hello](picture\hello.png)
+
+2.在命令行里传递一个文本文件名，读取文件中每一行的字符串信息，在控制台界面输入如下命令，可以将file.txt中的每一行信息生成一个QrCode。
+
+![fileCommand](picture/fileCommand.png)
+
+![file](picture\file.png)
+
+-f表示QrCode信息放在-f后的data\qrcode.txt文件中。生成的QrCode保存的文件名为信息所在行号三位数+信息的前四个字符的png格式。
+
+#### 代码总量
+
+100+行
+
+#### 工作时间
+
+约两天
+
+#### 结论
+
+基本完成了实验要求，通过此次实验，基本了解了Qrcode的生成方式，简单掌握了C#控制台编程。
+
